f9b3a40def0c017f9aec9001265ef6f22e00e083
shubhadip <shubhadip.maity@gmail.com>
2019-05-03 20:25:10 +0530

    coverage dec

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,9 +6,16 @@ export const capitalize = (input: string) => {
     const rem = input.slice(1,input.length)
     return first + rem
 }
+
 export const allLowercase = (input: string) => {
     if(!input){
         return ''
     }
     return input.toLowerCase()
+}
+export const allUpperCase = (input: string) => {
+    if(!input){
+        return ''
+    }
+    return input.toUpperCase()
 }
--- tests/unit/utils.spec.ts
+++ tests/unit/utils.spec.ts
@@ -1,4 +1,4 @@
-import { capitalize, allLowercase } from '../../src/utils/index'
+import { capitalize, allLowercase, allUpperCase } from '../../src/utils/index'
 
 describe('Test Capitalize Function', () => {
     it('input not avaliable', () => {
@@ -23,5 +23,13 @@ describe('All LowerCase Function', () => {
         const response = allLowercase('sd')
         expect(response).toBeTruthy()
         expect(response).toBe('sd')
+    })
+})
+
+describe('All UpperCase Function', () => {
+    it('input not avaliable', () => {
+        const response = allUpperCase('')
+        expect(response).toBeFalsy()
+        expect(response).toBe('')
     })
 })
